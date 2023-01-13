53914a73de59a094806564e6b9e6d6e6b49406e9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-13 18:39:33 +0100

    cd ..

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from enum import Enum
+
+class colors(Enum):
+    RED = 1,
+    GREEN = 2,
+    BLUE = 3
+
+
 if __name__ == '__main__':
     print("pyton 3.11")
+    print(colors.RED)
+
+    print(type(colors.RED))

46e909317402acf92ca989fb8785507199d1469d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-13 18:56:58 +0100

    cd ..

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from enum import Enum
+from enum import Enum, unique, auto
 
-class colors(Enum):
-    RED = 1,
+
+@unique
+class Colors(Enum):
+    RED = 10,
     GREEN = 2,
-    BLUE = 3
+    BLUE = 10
+
+
+def f0(a: tuple[str, int]) -> tuple[str, int]:
+    return "a", 1
 
 
 if __name__ == '__main__':
     print("pyton 3.11")
-    print(colors.RED)
+    print(Colors.RED)
 
-    print(type(colors.RED))
+    print(type(Colors.RED))
+
+    f0(a=("a", 1))
+
+    colorList = [Colors.RED, Colors.BLUE]
+    color0 = Colors.GREEN
+
+    if color0 in colorList:
+        print("Kolor znajduje się na liście")
+    else:
+        print("Nieprawidłowy kolor")
+
+    print(f"{Colors.RED.name}: {Colors.RED.value}")

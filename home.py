85354c2133b5602a02aaf4360171a83b4bbb2a86
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-20 19:14:48 +0100

    cd ..

--- home.py
+++ home.py
@@ -1,4 +1,18 @@
+from room import Room
 from salon import Salon
+
+
+class Home:
+    room_list : list
+    def __init__(self):
+        pass
+
+    def getRadiatorPower(self):
+        pass
+
+    def addRoom(self, room: Room):
+        pass
+
 
 if __name__ == '__main__':
     print("Dom")

91ea100c6dde052af014da858482520becdeb3c1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-01-27 19:18:43 +0100

    cd ..

--- room.py
+++ room.py
@@ -16,3 +16,7 @@ class Room(ABC):
     @abstractmethod
     def radiatorPower(self) -> float:
         pass
+
+    def printInfo(self):
+        print(
+            f"nazwa: {self.name}, powierzchnia: {self.roomArea()}, zapotrzebowanie energetyczne: {self.radiatorPower()} kWh")

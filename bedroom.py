b00794f5b00a19c7c9415259ab9187c8fc02ce91
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-02-03 18:25:28 +0100

    cd ..

--- bedroom.py
+++ bedroom.py
@@ -1,14 +1,16 @@
+from Figures2D.figure2D import Figure2D
 from room import Room
 
+
 class Bedroom(Room):
-    def __init__(self, a: float, b: float):
-        super().__init__(a, b)
+    def __init__(self, base: Figure2D):
+        super().__init__(base)
         self.name = "Łazienka"
         self.radiator_power_area = 0.5
 
     def roomArea(self):
+        self.base.area()
         return self.a * self.b
 
     def radiatorPower(self):
         return self.radiator_power_area * self.roomArea()
-

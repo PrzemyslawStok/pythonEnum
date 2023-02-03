d9f09aafbb6257e5d00098de433a4f322cb9925f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-02-03 19:18:33 +0100

    cd ..

--- figures/figure2d.py
+++ figures/figure2d.py
@@ -1,11 +1,14 @@
-
 class figure2d:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
     @staticmethod
     def printInfo():
         print("figure2d")
 
     def printArea(self):
-        print("area: ")
+        print(f"area: {self.a * self.b}")
 
     @classmethod
     def printArea_1(cls):
@@ -13,4 +16,8 @@ class figure2d:
 
 
 if __name__ == "__main__":
-    figure = figure2d()
+    figure = figure2d(5, 5)
+    figure1 = figure2d(10, 10)
+
+    figure.printArea()
+    figure1.printArea()

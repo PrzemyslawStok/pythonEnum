62726002b87636cc2fcb3115e6adc01c336bca34
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-02-03 19:15:53 +0100

    cd ..

--- figures/figure2d.py
+++ figures/figure2d.py
@@ -1,5 +1,16 @@
+
 class figure2d:
-    pass
+    @staticmethod
+    def printInfo():
+        print("figure2d")
+
+    def printArea(self):
+        pass
+
+    @classmethod
+    def printArea_1(cls):
+        pass
+
 
 if __name__ == "__main__":
     figure = figure2d()

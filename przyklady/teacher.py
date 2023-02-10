75e87081d56f4314a8f8f8aaaa4e99d444279071
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-02-10 19:27:01 +0100

    cd ..

--- przyklady/teacher.py
+++ przyklady/teacher.py
@@ -1,5 +1,6 @@
 from przyklady.person import Person
 
+
 class Teacher(Person):
-    def __init__(self, name: str, surname: str):
+    def __init__(self, name: str, surname: str, lectures: list[str]):
         super().__init__(name, surname)
